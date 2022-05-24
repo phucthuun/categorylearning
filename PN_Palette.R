2335afd737bb6c3b6056507837b9c8122b5cc86a
phucthuun <phuc.thu.uyen.nguyen@gmail.com>
2022-05-24 12:53:42 +0200

    Create PN_Palette.R

---
+++ PN_Palette.R
@@ -0,0 +1,38 @@
+# My palette
+
+# Type/Training condition (Malte's orig. palette)
+Palette_Type <- c('#8A8AEE','#ED8989') # (Malte's orig. palette)
+Palette_Type.Mixed <- '#ab8acc'
+Palette_Type <- c('#8E96E1','#E26A88') # (Phuc's palette)
+Palette_Type.Mixed <- '#7F587B'
+
+# Palette 1
+# https://coolors.co/palette/b61a13-e26a88-7f587b-8e96e1-46768b-94d197-8a9a5b-dcac7a-ee7808
+Palette <- c('#B61A13','#E26A88','#7F587B','#8E96E1','#46768B','#94D197','#8A9A5B','#DCAC7A','#EE7808')
+
+# 12 areas
+Palette_AreaAbs <- c('#363285', '#1e9dca', '#9fd6e5', '#da3893', '#f6897e', '#e32526',
+                     '#036b2d', '#3bb54f', '#90f3ab', '#f2de42', '#fd9727', '#8e5412')
+
+# Learning condition (con50 vs con50dn) * Instance type (training vs. novel)
+Palette_TypexInstance <- c('#5863A9','#8E96E1','#A73456','#E26A88')
+
+# Palette for dissimilarity types:
+# W-TT, W-TN, W-NN
+Palette_DissimilarityType <- c('#007DB9','#007B75')#'#CF5B8C')
+# Learning condition (con50 vs con50dn) * Dissimilarity type (W-TT vs. W-TN)
+Palette_TypexDissimilarityW <- c('#007B75','#54B4AD','#9A6119','#D9984F')
+# Learning condition (con50 vs con50dn) * Dissimilarity type (W-TN - W-TT)
+Palette_TypexDissimilarityGeneralization <- c('#339791','#B97C34')
+
+
+
+# Palette for sharedness
+# c('Category Label unique','Proper Name unique', 'Category Label shared','Proper Name shared')
+Palette_Sharedness = c('#8E96E1','#E26A88','#5863A9','#A73456')
+
+
+# Learning condition (con50 vs con50dn vs con50sc) * Dissimilarity type (W-TT vs. W-TN)
+Palette_TypexDissimilarityW <- c('#007B75','#54B4AD','#9A6119','#D9984F','#4B4C4B','#7F807F')
+# Learning condition (con50 vs con50dn) * Dissimilarity type (W-TN - W-TT)
+Palette_TypexDissimilarityGeneralization <- c('#54B4AD','#D9984F','#7F807F')
